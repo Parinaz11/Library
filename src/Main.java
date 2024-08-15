f01004975ffa7a49a70e2508819754f2746cef38
Parinaz11 <parinaz.akef@gmail.com>
2024-08-15 13:11:19 +0330

    first changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("--- Library ---");
+        System.out.println("Enter your choice:\n1) Reserve a Book\n2) Book Lists\n3) Reserve Status");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                reserveBook();
+                break;
+            case 2:
+                showBookList();
+                break;
+            case 3:
+                showReserveStatus();
+        }
+    }
+
+    public static void reserveBook() {
+        System.out.println("--- Reserve ---");
+        System.out.println("Enter the name of the book:");
+        Scanner scan = new Scanner(System.in);
+        String bookName = scan.nextLine();
+        System.out.println("Sorry, the book " + bookName + " is reserved.");
+    }
+
+    public static void showBookList() {
+        System.out.println("--- Our Books ---");
+    }
+
+    public static void showReserveStatus() {
+        System.out.println("--- Reserve Status ---");
     }
 }
