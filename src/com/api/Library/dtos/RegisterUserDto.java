42a2a4c6aa4fff8de4bce8a8ce73b1d529c2f48b
zeynabT <zey.teymoori@gmail.com>
2024-10-16 01:02:47 +0330

    add RegisterUserDto.java

---
+++ src/com/api/Library/dtos/RegisterUserDto.java
@@ -0,0 +1,11 @@
+package com.api.Library.dtos;
+
+public class RegisterUserDto {
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+    // getters and setters here...
+}
