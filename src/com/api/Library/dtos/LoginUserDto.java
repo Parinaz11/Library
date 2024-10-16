d7762ea2af3e49d830d2d30d71dc048c44b2057d
zeynabT <zey.teymoori@gmail.com>
2024-10-16 18:54:23 +0330

    update LoginUserDto.java for security

--- src/com/api/Library/dtos/LoginUserDto.java
+++ src/com/api/Library/dtos/LoginUserDto.java
@@ -2,8 +2,23 @@ package com.api.Library.dtos;
 
 public class LoginUserDto {
     private String email;
-
     private String password;
 
-    // getters and setters here...
+    // Getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
