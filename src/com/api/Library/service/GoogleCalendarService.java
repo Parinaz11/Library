a56342cf0d122be69b86d0325f2b9d154c816722
Parinaz11 <parinaz.akef@gmail.com>
2024-10-17 07:48:22 +0330

    all fixed

--- src/com/api/Library/service/GoogleCalendarService.java
+++ src/com/api/Library/service/GoogleCalendarService.java
@@ -48,7 +48,7 @@ public class GoogleCalendarService {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES =
-            Collections.singletonList(CalendarScopes.CALENDAR);
+            Collections.singletonList(CalendarScopes.CALENDAR_EVENTS);
     private static final String CREDENTIALS_FILE_PATH = "/client_secret_848394141846-8eab5krvapsjhvad8a98ugpabaahb28t.apps.googleusercontent.com.json";
 
     /**
@@ -76,15 +76,20 @@ public class GoogleCalendarService {
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        if (credential == null) {
+            throw new IllegalStateException("Credential is null");
+        }
         //returns an authorized Credential object.
         return credential;
     }
 
     public void createReservationEvent(String bookName, String userName, LocalDateTime startTime, LocalDateTime endTime) throws GeneralSecurityException, IOException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        System.out.println("BEFORE CREATING CALENDAR");
         Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+        System.out.println("AFTER CREATING CALENDAR");
 
         Event event = new Event()
                 .setSummary("Book Reservation: " + bookName)
@@ -140,87 +145,3 @@ public class GoogleCalendarService {
     }
 }
 
-
-
-//import com.google.api.client.auth.oauth2.Credential;
-//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-//import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-//import com.google.api.client.http.javanet.NetHttpTransport;
-//import com.google.api.client.json.JsonFactory;
-//import com.google.api.client.json.jackson2.JacksonFactory;
-//import com.google.api.client.util.store.FileDataStoreFactory;
-//import com.google.api.services.calendar.Calendar;
-//import com.google.api.services.calendar.CalendarScopes;
-//import com.google.api.services.calendar.model.Event;
-//import com.google.api.services.calendar.model.EventDateTime;
-//
-//import java.io.File;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.security.GeneralSecurityException;
-//import java.time.LocalDateTime;
-//import java.time.ZoneOffset;
-//import java.util.Collections;
-//import java.util.List;
-//
-//public class GoogleCalendarService {
-//
-//    private static final String APPLICATION_NAME = "Library Booking System";
-//    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-//    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-//
-//    /**
-//     * Global instance of the scopes required by this application.
-//     * If modifying these scopes, delete your previously saved tokens/ folder.
-//     */
-//    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_EVENTS);
-//    private static final String CREDENTIALS_FILE_PATH = "src/com/api/Library/presentation/client_secret_848394141846-8eab5krvapsjhvad8a98ugpabaahb28t.apps.googleusercontent.com.json";
-//
-//    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-//        // Load client secrets.
-//        FileReader reader = new FileReader(new File(CREDENTIALS_FILE_PATH));
-//        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, reader);
-//
-//        // Build flow and trigger user authorization request.
-//        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-//                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-//                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
-//                .setAccessType("offline")
-//                .build();
-//        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8081).build();
-//        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-//    }
-//
-//    public static Calendar getCalendarService() throws GeneralSecurityException, IOException {
-//        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-//        return new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-//                .setApplicationName(APPLICATION_NAME)
-//                .build();
-//    }
-//
-//    public static void createReservationEvent(String bookTitle, String username, LocalDateTime startTime, LocalDateTime endTime) throws Exception {
-//        Calendar service = getCalendarService();
-//
-//        Event event = new Event()
-//                .setSummary("Book Reservation: " + bookTitle)
-//                .setDescription("Reserved by: " + username);
-//
-//        EventDateTime start = new EventDateTime()
-//                .setDateTime(new com.google.api.client.util.DateTime(startTime.toInstant(ZoneOffset.UTC).toString()))
-//                .setTimeZone("UTC");
-//        event.setStart(start);
-//
-//        EventDateTime end = new EventDateTime()
-//                .setDateTime(new com.google.api.client.util.DateTime(endTime.toInstant(ZoneOffset.UTC).toString()))
-//                .setTimeZone("UTC");
-//        event.setEnd(end);
-//
-//        String calendarId = "primary"; // The primary calendar of the user
-//        event = service.events().insert(calendarId, event).execute();
-//        System.out.printf("Event created: %s\n", event.getHtmlLink());
-//    }
-//}
-//
